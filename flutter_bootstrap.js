adafaa55ea25b8330894393c11af924dd9a9f100
maulik <maulikkodala38@gmail.com>
2024-10-16 00:26:57 +0530

    ggg

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "361000547"
+    serviceWorkerVersion: "3764001949"
   }
 });
